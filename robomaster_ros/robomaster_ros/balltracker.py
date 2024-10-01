1f3ef4e9b90cf07440a3c8c1c40e102dc1547c21
BenkoDaniel <benko.dani01@gmail.com>
2024-10-01 21:50:24 +0200

    Non-working code added

--- robomaster_ros/robomaster_ros/balltracker.py
+++ robomaster_ros/robomaster_ros/balltracker.py
@@ -4,8 +4,9 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
-from geometry_msgs.msg import GimbalCommand
-
+from robomaster_msgs.msg import GimbalCommand
+from std_msgs.msg import Bool
+import time
 
 class BallTracker(Node):
 
@@ -19,7 +20,19 @@ class BallTracker(Node):
             '/camera/image_color',
             self.cb_camera,
             10)
+
         self.gimbal_pub = self.create_publisher(GimbalCommand, '/cmd_gimbal', 10)
+        self.gimbal_statepub = self.create_publisher(Bool, '/gimbal/engage', 10)
+        self.pic_center_x = 320
+        self.pic_center_y = 130
+        self.timer_period = 0.1
+        #self.timer = self.create_timer(self.timer_period, self.timer_callback)
+        #self.lastrcvtime = time.time() - 10000
+        self.rightspeed = 0.3
+        self.leftspeed = -0.3
+        self.upspeed = 0.3
+        self.downspeed = -0.3
+
 
     def cb_camera(self, msg):
         try:
@@ -34,6 +47,9 @@ class BallTracker(Node):
     def find_ball(self, image):
         results = self.yolomodel(image)
         df = results.pandas().xyxy[0]
+        x1, x2, = 320, 320
+        y1, y2 = 130, 130
+
         for _, row in df.iterrows():
 
             x1, y1, x2, y2 = int(row['xmin']), int(row['ymin']), int(row['xmax']), int(row['ymax'])
@@ -41,22 +57,46 @@ class BallTracker(Node):
             if row['name'] == "sports ball":
                 if row['confidence'] > 0.50:
                     cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    #center_x = int((x1 + x2) / 2)
+                    #center_y = int((y1 + y2) / 2)
+                    #print(center_x, center_y)
                     cv2.putText(image, f"{row['name']} {row['confidence']:.2f}", (x1, y1 - 5),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-
-        loop_rate = self.create_rate(100, self.get_clock())
-        speed = self.get_parameter('speed').get_parameter_value().double_value
+        #loop_rate = self.create_rate(100, self.get_clock())
         gimbal_msg = GimbalCommand()
-        gimbal_msg.pitch_speed = 0.0
-        gimbal_msg.jaw_speed = 0.0
-
         center_x = int((x1 + x2) / 2)
         center_y = int((y1 + y2) / 2)
-        dx =
+
+        dx = center_x - self.pic_center_x
+        dy = center_y - self.pic_center_y
+
+        if dx < 0:
+            gimbal_msg.yaw_speed = self.leftspeed
+        elif dx > 0:
+            gimbal_msg.yaw_speed = self.rightspeed
+        else:
+            gimbal_msg.yaw_speed = 0.0
+
+        if dy < 0:
+            gimbal_msg.pitch_speed = self.upspeed
+        elif dy > 0:
+            gimbal_msg.pitch_speed = self.downspeed
+        else:
+            gimbal_msg.pitch_speed = 0.0
+
+        self.gimbal_pub.publish(gimbal_msg)
 
 
         return image
+    def shutdown(self):
+        gimbal_msg = GimbalCommand()
+        gimbal_msg.pitch_speed = 0.0
+        gimbal_msg.yaw_speed = 0.0
+        self.gimbal_pub.publish(gimbal_msg)
+        gimbal_false = Bool()
+        gimbal_false.data = False
+        self.gimbal_statepub.publish(gimbal_false)
 
 
 def main(args=None):
@@ -65,6 +105,7 @@ def main(args=None):
     try:
         rclpy.spin(bt)
     finally:
+        bt.shutdown()
         bt.destroy_node()
         rclpy.shutdown()
 
