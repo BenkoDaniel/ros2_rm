ba27811a125a5679221c86053a222301807a265b
BenkoDaniel <benko.dani01@gmail.com>
2024-10-02 18:17:31 +0200

    Code fixes. Ball coordinates published to detected_ball topic

--- robomaster_ros/robomaster_ros/balltracker.py
+++ robomaster_ros/robomaster_ros/balltracker.py
@@ -4,9 +4,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
-from robomaster_msgs.msg import GimbalCommand
-from std_msgs.msg import Bool
-import time
+from geometry_msgs.msg import Point
 
 class BallTracker(Node):
 
@@ -21,34 +19,37 @@ class BallTracker(Node):
             self.cb_camera,
             10)
 
-        self.gimbal_pub = self.create_publisher(GimbalCommand, '/cmd_gimbal', 10)
-        self.gimbal_statepub = self.create_publisher(Bool, '/gimbal/engage', 10)
-        self.pic_center_x = 320
-        self.pic_center_y = 130
-        self.timer_period = 0.1
-        #self.timer = self.create_timer(self.timer_period, self.timer_callback)
-        #self.lastrcvtime = time.time() - 10000
-        self.rightspeed = 0.3
-        self.leftspeed = -0.3
-        self.upspeed = 0.3
-        self.downspeed = -0.3
-
+        self.ballpub = self.create_publisher(Point, "/detected_ball", 1)
 
     def cb_camera(self, msg):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-            im_with_ballmarker = self.find_ball(cv_image)
+            im_with_ballmarker, ball_x, ball_y = self.find_ball(cv_image)
             cv2.imshow("Camera", im_with_ballmarker)
             cv2.waitKey(1)
+            point = Point()
+            rows = float(cv_image.shape[0])
+            cols = float(cv_image.shape[1])
+            center_x    = 0.5*cols
+            center_y    = 0.5*rows
+            point.x = (ball_x - center_x)/center_x
+            point.y = (ball_y - center_y)/center_y
+            if point.x > 0:
+                self.ballpub.publish(point)
+
+
 
         except CvBridgeError as e:
             self.get_logger().error(f"CvBridge error: {e}")
 
+
+
+
+
     def find_ball(self, image):
         results = self.yolomodel(image)
         df = results.pandas().xyxy[0]
-        x1, x2, = 320, 320
-        y1, y2 = 130, 130
+        center_x, center_y = 0, 0
 
         for _, row in df.iterrows():
 
@@ -57,46 +58,13 @@ class BallTracker(Node):
             if row['name'] == "sports ball":
                 if row['confidence'] > 0.50:
                     cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                    #center_x = int((x1 + x2) / 2)
-                    #center_y = int((y1 + y2) / 2)
+                    center_x = int((x1 + x2) / 2)
+                    center_y = int((y1 + y2) / 2)
                     #print(center_x, center_y)
                     cv2.putText(image, f"{row['name']} {row['confidence']:.2f}", (x1, y1 - 5),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-        #loop_rate = self.create_rate(100, self.get_clock())
-        gimbal_msg = GimbalCommand()
-        center_x = int((x1 + x2) / 2)
-        center_y = int((y1 + y2) / 2)
-
-        dx = center_x - self.pic_center_x
-        dy = center_y - self.pic_center_y
-
-        if dx < 0:
-            gimbal_msg.yaw_speed = self.leftspeed
-        elif dx > 0:
-            gimbal_msg.yaw_speed = self.rightspeed
-        else:
-            gimbal_msg.yaw_speed = 0.0
-
-        if dy < 0:
-            gimbal_msg.pitch_speed = self.upspeed
-        elif dy > 0:
-            gimbal_msg.pitch_speed = self.downspeed
-        else:
-            gimbal_msg.pitch_speed = 0.0
-
-        self.gimbal_pub.publish(gimbal_msg)
-
-
-        return image
-    def shutdown(self):
-        gimbal_msg = GimbalCommand()
-        gimbal_msg.pitch_speed = 0.0
-        gimbal_msg.yaw_speed = 0.0
-        self.gimbal_pub.publish(gimbal_msg)
-        gimbal_false = Bool()
-        gimbal_false.data = False
-        self.gimbal_statepub.publish(gimbal_false)
+        return image, center_x, center_y
 
 
 def main(args=None):
@@ -105,7 +73,6 @@ def main(args=None):
     try:
         rclpy.spin(bt)
     finally:
-        bt.shutdown()
         bt.destroy_node()
         rclpy.shutdown()
 
