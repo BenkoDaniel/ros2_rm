3a116ae06493f236115636070adbc90ef59a5cc5
BenkoDaniel <benko.dani01@gmail.com>
2024-10-15 22:09:32 +0200

    simulation progress

--- robomaster_ros/launch/gazebo_model.launch.py
+++ robomaster_ros/launch/gazebo_model.launch.py
@@ -1,37 +1,20 @@
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-import xacro
+from launch.actions import ExecuteProcess
+import launch_ros
 
 def generate_launch_description():
-    robotXacroname='robomaster_s1'
-    namePackage = 'robomaster_ros'
-    modelFileRelativePath = 'urdf/urdf/robomaster_s1.urdf.xacro'
-    worldFileRealtivePath = 'Gazebo/emptyworld.world'
-    pathModelFile = os.path.join(get_package_share_directory(namePackage), modelFileRelativePath)
-    pathWorldFile = os.path.join(get_package_share_directory(namePackage), worldFileRealtivePath)
-    robotDescription = xacro.process_file(pathModelFile).toxml()
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='robomaster_ros').find('robomaster_ros')
+    world_file = os.path.join(pkg_share, 'gazebo/simulation.sdf')
 
-    gazebo_rosPackageLaunch=PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('gazebo_ros'),'launch','model.launch.py'))
-    gazeboLaunch = IncludeLaunchDescription(gazebo_rosPackageLaunch, launch_arguments={'world':pathWorldFile}.items())
-
-    spawnModelNode = Node(package='robomaster_ros', executable='spawn_entity.py', arguments=['-topic','robot_description','-entity',robotXacroname], output='screen')
-
-    nodeRpbotStatePublisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robotDescription,
-                     'use_sim_time': True}]
-    )
-
-    launchDescriptionObject = LaunchDescription()
-
-    launchDescriptionObject.add_action(gazeboLaunch)
-    launchDescriptionObject.add_action(spawnModelNode)
-    launchDescriptionObject.add_action(nodeRpbotStatePublisher)
-
-    return launchDescriptionObject
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', world_file, '-u'],
+            shell=True
+        ),
+        launch_ros.actions.Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=['-entity', 'robomaster_ros', '-topic', 'robot_description'],
+            output='screen')
+    ])
