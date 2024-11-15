f89dbfc7ebfa4d0c45b04fa6ff25680afd15367d
BenkoDaniel <benko.dani01@gmail.com>
2024-11-15 10:02:24 +0100

    modification for controlling

--- robomaster_description/launch/controller.launch.py
+++ robomaster_description/launch/controller.launch.py
@@ -25,6 +25,23 @@ def generate_launch_description():
 
     robot_description = Parameter("robot_description", value=urdf_content)
 
+    spawn_entity_node = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=[
+            '-entity', 'robomaster',
+            '-file', urdf_content
+        ],
+        output="screen"
+    )
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description]
+    )
+
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
@@ -42,12 +59,6 @@ def generate_launch_description():
         executable="spawner",
         arguments=["gim_controller"],
     )
-    diff_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diff_controller"],
-    )
-
     command_converter = Node(
         package="robomaster_ros",
         executable="gimbal_command_converter"
@@ -61,18 +72,13 @@ def generate_launch_description():
             on_exit=[gimbal_controller_spawner]
         )
     )
-    delay_diff_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[diff_controller_spawner]
-        )
-    )
 
 
     return LaunchDescription([
+        spawn_entity_node,
+        robot_state_publisher,
         controller_manager,
         joint_state_broadcaster_spawner,
         delay_gimbal_controller_spawner_after_joint_state_broadcaster_spawner,
-        delay_diff_controller_spawner_after_joint_state_broadcaster_spawner,
         command_converter
     ])
