4a29752b1473eb009f01b2faa92006ee011ac4d6
BenkoDaniel <benko.dani01@gmail.com>
2025-03-12 21:19:27 +0100

    environment building proccess

--- robomaster_ros/robomaster_ros/robomaster_soccer_env.py
+++ robomaster_ros/robomaster_ros/robomaster_soccer_env.py
@@ -9,8 +9,7 @@ from gazebo_msgs.msg import ModelState, EntityState, ModelStates, LinkStates
 from gazebo_msgs.srv import SetEntityState
 from std_srvs.srv import Empty
 
-max_lifes = 5
-
+ball_position = np.array([0, 0], float)
 
 class RobomasterSoccerEnv(gym.Env):
     '''Custom Environment that follows the gym interface'''
@@ -20,7 +19,7 @@ class RobomasterSoccerEnv(gym.Env):
         self.action_space = spaces.Box(low=np.array([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0]),
                                        high=np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0]),
                                        dtype=np.float32) #the whole twist msg
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(14, 0), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(10,), dtype=np.float32)
         #robot1_x, robot1_y,
         #robot2_x, robot2_y,
         #ball_x, ball_y, ball_z
@@ -28,8 +27,29 @@ class RobomasterSoccerEnv(gym.Env):
         rclpy.init()
         self.node = Node("Robomaster_Soccer_env")
 
-        self.robot1_vel = np.array([0, 0, 0, 0, 0, 0], float)
-        self.robot2_vel = np.array([0, 0, 0, 0, 0, 0], float)
+        #to move a robot to initial position
+        self.set_robot_1_state = EntityState()
+        self.set_robot_1_state.pose.position.x = -0.5
+        self.set_robot_1_state.pose.position.y = 0.0
+        self.set_robot_1_state.pose.position.z = 0.0
+        self.robot_1_state = SetEntityState.Request()
+
+        self.set_robot_2_state = EntityState()
+        self.set_robot_2_state.pose.position.x = 0.5
+        self.set_robot_2_state.pose.position.y = 0.0
+        self.set_robot_2_state.pose.position.z = 0.0
+        self.robot_2_state = SetEntityState.Request()
+
+        self.set_sphere_state = EntityState()
+        self.set_sphere_state.name = "ball"
+        self.set_sphere_state.pose.position.x = 0.0
+        self.set_sphere_state.pose.position.y = 0.0
+        self.set_sphere_state.pose.position.z = 0.175
+        self.set_sphere_state.pose.orientation.x = 0.0
+        self.set_sphere_state.pose.orientation.y = 0.0
+        self.set_sphere_state.pose.orientation.z = 0.0
+        self.set_sphere_state.pose.orientation.w = 1.0
+        self.sphere_state = SetEntityState.Request()
 
         self.set_state = self.node.create_client(SetEntityState, "/gazebo/set_entity_state")
         self.pause = self.node.create_client(Empty, "/pause_physics")
@@ -45,45 +65,104 @@ class RobomasterSoccerEnv(gym.Env):
         self.robot1_ball_sub = self.node.create_subscription(Point, "/robot1/detected_ball", self.robot1_ball_callback, 10)
         self.robot2_ball_sub = self.node.create_subscription(Point, "/robot2/detected_ball", self.robot2_ball_callback, 10)
 
-        self.set_sphere_state = EntityState()
-        self.set_sphere_state.name = "ball"
-        self.set_sphere_state.pose.position.x = 0.0
-        self.set_sphere_state.pose.position.y = 0.0
-        self.set_sphere_state.pose.position.z = 0.2
-        self.sphere_state = SetEntityState.Request()
-
-        self.robot1_life = max_lifes
-        self.robot2_life = max_lifes
-
         self.t = 0
         self.t_limit = 6000
 
         self.robot1_state = None
         self.robot2_state = None
-        self.robot1_ball_state = None
-        self.robot2_ball_state = None
+        self.robot1_ball_relative = None
+        self.robot2_ball_relative = None
+        self.done = False
+        self.reward = 0
 
+        # robot1_odom (4),  robot2_odom (4), ??
+        # robot1_ball_dx, robot1_ball_dy, robot2_ball_dx, robot2_ball_dy  - relative coordinates
+        # ball_x, ball_y  - from gazebo
 
+        self.observation = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        #(x_agent, y_agent, x_agent_next, y_agent_next,
-        # x_ball, y_ball, x_ball_next, y_ball_next,
-        # x_opponent, y_opponent, x_opponent_next, y_opponent_next)
-
-        self.robot1_obs = np.array([])
 
     def robot1_odom_callback(self, msg):
-        self.robot1_state = (msg.pose.pose.position.x, msg.pose.pose.position.y, msg.twist.twist.linear.x, msg.twist.twist.linear.y)
+        self.robot1_state = [msg.pose.pose.position.x, msg.pose.pose.position.y,
+                             msg.twist.twist.linear.x, msg.twist.twist.linear.y]
 
     def robot2_odom_callback(self, msg):
-        self.robot2_state = (msg.pose.pose.position.x, msg.pose.pose.position.y, msg.twist.twist.linear.x, msg.twist.twist.linear.y)
+        self.robot2_state = [msg.pose.pose.position.x, msg.pose.pose.position.y,
+                             msg.twist.twist.linear.x, msg.twist.twist.linear.y]
 
     def robot1_ball_callback(self, msg):
-        self.robot1_ball_state = (msg.x, msg.y)
+        self.robot1_ball_relative = [msg.x, msg.y]
 
     def robot2_ball_callback(self, msg):
-        self.robot2_ball_state = (msg.x, msg.y)
+        self.robot2_ball_relative = [msg.x, msg.y]
 
     def step(self, action):
+        global ball_position
+        self.t += 1
+
+        robot1_command = Twist()
+        robot2_command = Twist()
+
+        robot1_command.linear.x = action[0]
+        robot1_command.linear.y = action[1]
+        robot1_command.angular.z = action[2]
+
+        robot2_command.linear.x = action[3]
+        robot2_command.linear.y = action[4]
+        robot2_command.angular.z = action[5]
+
+        self.robot1_vel_publisher.publish(robot1_command)
+        self.robot2_vel_publisher.publish(robot2_command)
+
+        self.node.get_clock().sleep_for(rclpy.duration.Duration(seconds=0.1))
+
+        while not self.unpause.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        try:
+            self.unpause.call_async(Empty.Request())
+        except:
+            self.get_logger().info("/unpause_physics service call failed")
+
+        while not self.pause.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        try:
+            self.pause.call_async(Empty.Request())
+        except rclpy.ServiceException as e:
+            self.get_logger().info("/gazebo/pause_physics service call failed")
+
+        if abs(ball_position[0]) > 1.55:
+            if ball_position[0] < -1.55:
+                self.robot1_life -= 1
+                self.reward = -100
+                self.get_logger().info('ROBOT1 LOST A POINT!')
+            elif ball_position[0] > 1.55:
+                self.robot2_life -= 1
+                self.reward = 1000
+                self.get_logger().info('ROBOT1 GET A POINT!')
+            self.reset()
+        else:
+            self.reward = 0
+
+        if self.t >= self.t_limit:
+            self.done = True
+
+        self.observation = np.array([
+            self.robot_1_state[0],
+            self.robot_1_state[1],
+            self.robot_1_state[2],
+            self.robot_1_state[3],
+            self.robot_2_state[0],
+            self.robot_2_state[1],
+            self.robot_2_state[2],
+            self.robot_2_state[3],
+            self.robot1_ball_relative[0],
+            self.robot1_ball_relative[1],
+            self.robot2_ball_relative[0],
+            self.robot2_ball_relative[1],
+            ball_position[0],
+            ball_position[1]
+        ])
+
         return self.observation, self.reward, self.done, self.info
 
     def reset(self):
@@ -95,3 +174,21 @@ class RobomasterSoccerEnv(gym.Env):
     def close(self):
         ...
 
+
+class GetBallPosition(Node):
+
+    def __init__(self):
+        super().__init__('get_ball_position')
+        self.subscription = self.create_subscription(
+            ModelStates,
+            '/gazebo/model_states',
+            self.ball_pos_callback,
+            10)
+
+    def ball_pos_callback(self, data):
+        global ball_position
+
+        unit_sphere_id = data.name.index('unit_sphere')
+
+        ball_position[0] = data.pose[unit_sphere_id].position.x
+        ball_position[1] = data.pose[unit_sphere_id].position.y
