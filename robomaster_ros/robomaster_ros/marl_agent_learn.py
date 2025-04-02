36bd41e9cf377b8c3905cac9b231177f49969d6f
BenkoDaniel <benko.dani01@gmail.com>
2025-04-02 22:53:59 +0200

    training script progress

--- robomaster_ros/robomaster_ros/marl_agent_learn.py
+++ robomaster_ros/robomaster_ros/marl_agent_learn.py
@@ -1,25 +1,28 @@
 import os
 import time
-import supersuit as ss
-from stable_baselines3.common.vec_env import VecMonitor, DummyVecEnv
-from pettingzoo.utils import parallel_to_aec
-import gymnasium as gym
-from gymnasium.wrappers import OrderEnforcing
-from stable_baselines3 import PPO, A2C
 from robomaster_soccer_env import RobomasterSoccerEnv
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
 
-
-PPO_models_dir = f"models/PPO-{int(time.time())}"
+PPO_robot1_models_dir = f"models/robot1/PPO-{int(time.time())}"
+PPO_robot2_models_dir = f"models/robot2/PPO-{int(time.time())}"
 #A2C_models_dir = f"models/A2C-{int(time.time())}"
 
-PPO_logs_dir = f"logs/PPO-{int(time.time())}"
+PPO_robot1_logs_dir = f"logs/robot1/PPO-{int(time.time())}"
+PPO_robot2_logs_dir = f"logs/robot2/PPO-{int(time.time())}"
 #A2C_logs_dir = f"logs/A2C-{int(time.time())}"
 
-if not os.path.exists(PPO_models_dir):
-    os.makedirs(PPO_models_dir)
+if not os.path.exists(PPO_robot1_models_dir):
+    os.makedirs(PPO_robot1_models_dir)
 
-if not os.path.exists(PPO_logs_dir):
-    os.makedirs(PPO_logs_dir)
+if not os.path.exists(PPO_robot2_models_dir):
+    os.makedirs(PPO_robot2_models_dir)
+
+if not os.path.exists(PPO_robot1_logs_dir):
+    os.makedirs(PPO_robot1_logs_dir)
+
+if not os.path.exists(PPO_robot2_logs_dir):
+    os.makedirs(PPO_robot2_logs_dir)
 
 #if not os.path.exists(A2C_models_dir):
 #    os.makedirs(A2C_models_dir)
@@ -29,15 +32,62 @@ if not os.path.exists(PPO_logs_dir):
 
 
 env = RobomasterSoccerEnv()
-env = ss.pettingzoo_env_to_vec_env_v1(env)
-env = ss.concat_vec_envs_v1(env, 1, num_cpus=1, base_class="stable_baselines3")
+wrapped_env = make_vec_env(lambda: env, n_envs=1)
 
-PPO_model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=PPO_logs_dir, ent_coef=0.01, learning_rate=0.0005, gamma=0.99, clip_range=0.2)
-#A2C_model = A2C("MlpPolicy", env, verbose=1, tensorboard_log=A2C_logs_dir)
+policies = {}
+policies["robot1"] = PPO(
+        "MlpPolicy",
+        env=wrapped_env,
+        verbose=1,
+        tensorboard_log=PPO_robot1_logs_dir,
+        policy_kwargs=dict(
+            net_arch=[64, 64]
+        )
+    )
+policies["robot2"] = PPO(
+        "MlpPolicy",
+        env=wrapped_env,
+        verbose=1,
+        tensorboard_log=PPO_robot2_logs_dir,
+        policy_kwargs=dict(
+            net_arch=[64, 64]
+        )
+    )
+
+episode_rewards = {agent: [] for agent in env.possible_agents}
 
 timesteps = 10000
-for i in range(1, 100):
-    PPO_model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name="PPO")
-    PPO_model.save(f"{PPO_models_dir}/{timesteps*i}")
+for i in range(timesteps):
+    obs = wrapped_env.reset()
+    done = False
+    episode_reward = {
+        "robot1": 0,
+        "robot2": 0
+    }
+    while not done:
+        actions = {}
+        for agent in env.possible_agents:
+            action, _ = policies[agent].predict(obs[agent])
+            actions[agent] = action
+        observations, rewards, terminations, truncations, info = wrapped_env.step(actions)
 
+        for agent in env.possible_agents:
+            episode_reward[agent] += rewards[agent]
+
+        for agent in env.possible_agents:
+            policies[agent].learn(
+                total_timesteps=100,
+                reset_num_timesteps=False
+            )
+        done = terminations["robot1"] or terminations["robot2"] or truncations["robot1"] or truncations["robot2"]
+        obs = observations
+    
+    for agent in env.possible_agents:
+            episode_rewards[agent].append(episode_reward[agent])
+            print(f"Episode {i+1}, Agent {agent} reward: {episode_reward[agent]:.2f}")
+    policies["robot1"].save(f"{PPO_robot1_models_dir}/{timesteps*i}")
+    policies["robot2"].save(f"{PPO_robot2_models_dir}/{timesteps*i}")
+
+
+env.stop()
 env.close()
